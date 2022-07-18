7cf910778fc1721a1ace53e391edd7c0d33fa583
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-07-18 15:07:08 -0300

    Initial commit

---
+++ src/CommissionCalculation.Test/CommissionCalculation.Test.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using FluentAssertions;
+using System.IO;
+using CommissionCalculation;
+
+namespace CommissionCalculation.Test;
+
+public class CommissionCalculatorTest
+{
+    [Theory(DisplayName = "Deve calcular corretamente o salário e armazenar o resultado no atributo FinalSalary.")]
+    [InlineData(1200, 2, 60000, 3500)]
+    public void TestCalculateFinalSalary(decimal fixedSalary, int amountCarsSold, decimal totalSalesValue, decimal expectedFinalSalary)
+    {
+        throw new NotImplementedException();
+    }
+
+    [Theory(DisplayName = "Deve exibir corretamente a mensagem de salário final.")]
+    [InlineData("Nome Contribuidor", "janeiro", new string[] {
+        "O colaborador Nome Contribuidor neste mês de janeiro obteve o salário final de R$0.00 referente à:",
+        "SALARIO FIXO: R$0.00",
+        "TOTAL DE CARROS VENDIDOS: 0",
+        "VALOR TOTAL DE VENDAS NO MES: R$0.00",
+        "COMISSÃO POR CARROS VENDIDOS: R$0.00",
+        "COMISSÃO DE 3% DO TOTAL DE VENDAS: R$0.00"
+    })]
+    public void TestShowFinalSalary(string contributorName, string month, string[] expectedMessage)
+    {
+        throw new NotImplementedException();
+    }
+}
