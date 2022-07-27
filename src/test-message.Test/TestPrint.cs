d47d81a49506d53b348a9a2a117322cc4af3a90e
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-27 11:45:27 -0300

    feat: add test TestPrint

--- src/test-message.Test/TestPrint.cs
+++ src/test-message.Test/TestPrint.cs
@@ -8,18 +8,22 @@ namespace test_message.Test;
 public class TestPrint
 {
     [Theory]
-    [InlineData()]
+    [InlineData("Olá Mundo", "Olá Mundo")]
+    [InlineData("Hello World", "Hello World")]
+    [InlineData("testando", "testando")]
+    [InlineData("test", "test")]
+    [InlineData("xablau", "xablau")]
     public void TestPrintMsgSucess(string Send, string Expected)
     {
         using (var NewOutput = new StringWriter())
         {
             Console.SetOut(NewOutput);
 
-            Message.PrintMsg();
+            Message.PrintMsg(Send);
 
             string result = NewOutput.ToString().Trim();
 
-            result.Should();      
+            result.Should().Be(Expected);      
         }
     }
 }
