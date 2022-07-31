a92dc7f8be1422446638403e9a93aba8edb81e7f
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-31 15:18:26 -0300

    feat: implemented MonthName

--- src/month-name/MonthNameInCalendar.cs
+++ src/month-name/MonthNameInCalendar.cs
@@ -8,6 +8,63 @@ public class MonthNameInCalendar
     /// <returns> Nome do mês</returns>
     public static string MonthName(int month)
     {
-        throw new NotImplementedException();
+        string monthIndicate;
+
+        switch(month)
+        {
+            case 1:
+                monthIndicate = "Janeiro";
+                break;
+            
+            case 2:
+                monthIndicate = "Fevereiro";
+                break;
+            
+            case 3:
+                monthIndicate = "Março";
+                break;
+            
+            case 4:
+                monthIndicate = "Abril";
+                break;
+            
+            case 5:
+                monthIndicate = "Maio";
+                break;
+            
+            case 6:
+                monthIndicate = "Junho";
+                break;
+            
+            case 7:
+                monthIndicate = "Julho";
+                break;
+            
+            case 8:
+                monthIndicate = "Agosto";
+                break;
+
+            case 9:
+                monthIndicate = "Setembro";
+                break;
+
+            case 10:
+                monthIndicate = "Outubro";
+                break;
+
+            case 11:
+                monthIndicate = "Novembro";
+                break;
+
+            case 12:
+                monthIndicate = "Dezembro";
+                break;
+
+            default:
+                monthIndicate = "inválido";
+                break;
+        }
+
+        return monthIndicate;
     }
 }
