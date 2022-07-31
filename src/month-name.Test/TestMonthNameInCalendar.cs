110515560243154cf14d2f69b1928793000dd3c0
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-31 15:25:30 -0300

    test: implmented TestMonthNameInCalendarSucess and TestMonthNameInCalendarOutOfRange

--- src/month-name.Test/TestMonthNameInCalendar.cs
+++ src/month-name.Test/TestMonthNameInCalendar.cs
@@ -8,15 +8,20 @@ public class TestMonthNameInCalendar
 {
     [Theory]
     [InlineData(1, "Janeiro")]
+    [InlineData(2, "Fevereiro")]
+    [InlineData(3, "Março")]
+    [InlineData(4, "Abril")]
     public void TestMonthNameInCalendarSucess(int month, string expected)
     {
-        throw new NotImplementedException();
+        var result = MonthNameInCalendar.MonthName(month);
+        result.Should().Be(expected);
     }
 
     [Theory]
     [InlineData(0, "inválido")]
     public void TestMonthNameInCalendarOutOfRange(int month, string expected)
     {
-        throw new NotImplementedException();
+        var result = MonthNameInCalendar.MonthName(month);
+        result.Should().Be(expected);
     }
 }
