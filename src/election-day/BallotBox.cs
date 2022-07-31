0e3426f2c25ceb79787faa83c28d5e05c62ea7af
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-31 18:31:54 -0300

    feat: implemented methods for BallotBox

--- src/election-day/BallotBox.cs
+++ src/election-day/BallotBox.cs
@@ -9,17 +9,57 @@ namespace election_day
         
         public int GetCountVoters()
         {
-            throw new NotImplementedException();
+            var numberInsert = 0;
+            var convertible = false;
+
+            do
+            {
+                Console.Write("O número deve ser um inteiro maior que zero");
+                var response = Console.ReadLine();
+                convertible = int.TryParse(response, out numberInsert);
+
+            }
+            while(!convertible);
+
+            if (numberInsert <= 0) return 1;
+            return numberInsert;
         }
 
         public void Start(int countVoters)
         {
-           throw new NotImplementedException();
+            for (var i = 0; i < countVoters; i++)
+            {
+                Console.Write("Informe o número do candidato de 1 a 3, ou tecle qualquer outro digito para votar nulo: ");
+                var vote = GetCountVoters();
+
+                switch (vote)
+                {
+                    case 1:
+                        receivedOption1 ++;
+                        break;
+                    case 2:
+                        receivedOption2 ++;
+                        break;
+                    case 3:
+                        receivedOption3 ++;
+                        break;
+                    default:
+                        optionNull ++;
+                        break;
+                }
+
+                Console.Write("Voto registrado: " + vote.ToString());
+            }
+            
         }
 
         public void PrintResult()
         {
-           throw new NotImplementedException();
+            Console.Write("Votação finalizada!!!!");
+            Console.Write("A opção 1 recebeu: " + receivedOption1 + " voto(s)");
+            Console.Write("A opção 2 recebeu: " + receivedOption2 + " voto(s)");
+            Console.Write("A opção 3 recebeu: " + receivedOption3 + " voto(s)");
+            Console.Write("Total de votos anulados: " + optionNull + " voto(s)");
         }
     }
 }
