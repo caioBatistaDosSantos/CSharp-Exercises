12d4f2a431206816f6e5c4e4b776aeb67cbc4b61
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-08-12 17:06:01 -0300

    Initial commit

---
+++ src/employee-registration/Employee.cs
@@ -0,0 +1,42 @@
+﻿namespace employee_registration;
+
+________ class Employee
+{
+    ________ class Account
+    {
+        ________ string _account;
+        ________ string _digit;
+
+        ________ Account(string account, string digit)
+        {
+            _account = account;
+            _digit = digit;
+        }
+    };
+
+    ________ string Name;
+    ________ double _salary;
+
+    ________ Account? _employeeAccount;
+
+    ________ Employee(string name, double salary)
+    {
+        Name = name;
+        _salary = salary;
+    }
+
+    ________ void setAccount(string account, string digit)
+    {
+        _employeeAccount = new Account(account, digit);
+    }
+
+    ________ void Print()
+    {
+        Console.WriteLine($"Olá meu nome é {Name}");
+    }
+
+    ________ void Pay()
+    {
+        Console.WriteLine($"{Name} foi pago em {_salary}");
+    }
+}
