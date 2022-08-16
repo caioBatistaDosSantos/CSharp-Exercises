0ebd662e90b44ac7a9c7782bb4b483a44db51914
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-08-16 19:24:55 -0300

    Initial commit

---
+++ src/DateHandler/Schedule.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+namespace DataHandler;
+
+public class Schedule : IScheduleFormat
+{
+    public DateTime[] nextEvents { get; }
+
+    public Schedule()
+    {
+        CultureInfo.CurrentCulture = new CultureInfo("pt-BR", false);
+        nextEvents = new DateTime[10];
+    }
+
+    public string NextEvent()
+    {
+        if(nextEvents[0] == new DateTime()) return "Não há eventos marcados";
+        else                                return nextEvents[0].ToString("D");
+    }
+
+    public void AddEventAtDays(int days)
+    {        
+        for (int index = 0; index < nextEvents.Length-1; index++)
+        {
+            if(nextEvents[index] == new DateTime())
+            {            
+                nextEvents[index] = (DateTime.Now).AddDays(days);
+                return;
+            }   
+        }
+        throw new ArgumentException("Não há espaço disponível para o evento.");
+    }
+
+    public void RemoveFirstEvent()
+    {
+        throw new NotImplementedException();
+    }
+
+}
