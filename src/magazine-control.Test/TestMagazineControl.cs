7e23bddd5e0581b6452cde8d95e3a5a3ffc6385f
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-08-23 18:03:45 -0300

    Initial commit

---
+++ src/magazine-control.Test/TestMagazineControl.cs
@@ -0,0 +1,51 @@
+using Xunit;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+
+namespace magazine_control.Test;
+
+public class TestMagazineControl
+{
+    [Theory(DisplayName = "Deveria adicionar a revista na lista")]
+    [MemberData(nameof(TestSaveMagazineData))]
+    public void TestSaveMagazine(List<string> magazines, string name, List<string> expectedMagazines, int expectedReturn)
+    {
+        throw new NotImplementedException();
+    }
+    public static TheoryData<List<string>, string, List<string>, int> TestSaveMagazineData = new()
+    {
+        {
+            new List<string>(){ "Mundo TI", "Natureza Espetacular" },
+            "Escola Nova",
+            new List<string>(){ "Mundo TI", "Natureza Espetacular", "Escola Nova" },
+            2
+        },
+        {
+            new List<string>(){ "Mundo TI", "Natureza Espetacular" }, 
+            "Casa Bela",
+            new List<string>(){ "Mundo TI", "Natureza Espetacular", "Casa Bela" },
+            2
+        },
+        {
+            new List<string>(){ "Natureza Espetacular" },
+            "Aviação Civil",
+            new List<string>(){ "Natureza Espetacular", "Aviação Civil" },
+            1
+        },
+    };
+
+
+    [Theory(DisplayName = "Deveria retornar o índice da revista")]
+    [MemberData(nameof(TestFindMagazinePositionData))]
+    public void TestFindMagazinePosition(List<string> magazines, string name, int expectedValue)
+    {
+        throw new NotImplementedException();
+    }
+    public static TheoryData<List<string>, string, int> TestFindMagazinePositionData = new TheoryData<List<string>, string, int>
+    {
+        { new List<string>(){ "Mundo TI", "Escola Nova", "Casa Bonita", "Natureza Espetacular" }, "Escola Nova", 1 },
+        { new List<string>(){ "Mundo TI", "Escola Nova", "Casa Bonita", "Natureza Espetacular" }, "Casa Bela", -1 },
+        { new List<string>(){ "Mundo TI", "Escola Nova", "Casa Bonita", "Natureza Espetacular" }, "Aviação Civil", -1 },
+    };
+}
---
+++ src/magazine-control/MagazineControl.cs
@@ -0,0 +1,30 @@
+﻿namespace magazine_control;
+public class MagazineControl
+{
+    public List<string> Magazines;
+
+    public MagazineControl(List<string> magazines)
+    {
+        Magazines = magazines;
+    }
+
+    /// <summary>
+    /// This function add a magazine to list and return the index.
+    /// </summary>
+    /// <param name="name">Magazine name</param>
+    /// <returns>Added item index.</returns>
+    public int SaveMagazine(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// This function search for a magazine and return the index.
+    /// </summary>
+    /// <param name="name">Magazine name</param>
+    /// <returns>Item index.</returns>
+    public int FindMagazinePosition(string name)
+    {
+        throw new NotImplementedException();
+    }
+}
