a47d4ad728d87f93036d406d14d00d7900897df7
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-08-24 23:48:12 -0300

    feat: add class Cow

--- src/farm-classes/Animals/Cow.cs
+++ src/farm-classes/Animals/Cow.cs
@@ -1,3 +1,17 @@
 ﻿namespace Animals;
 
-// Crie a sua classe Cow aqui
+class Cow
+{
+    Guid Id = Guid.NewGuid();
+    int Weight;
+    string Breed;
+
+    public Cow(int weight, string breed)
+    {
+        if (weight <= 0 || breed == null || breed == "")
+            throw new ArgumentException("Os dados informados incorretamente");
+        
+        Weight = weight;
+        Breed = breed;
+    }
+}
