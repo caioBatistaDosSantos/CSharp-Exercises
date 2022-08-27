bd06823e478b538d03b37fb712a20e58f62a7336
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-08-27 20:12:11 -0300

    feat/test: class Rectangle

--- src/Geometry/Rectangle.cs
+++ src/Geometry/Rectangle.cs
@@ -1,4 +1,23 @@
 ﻿namespace Geometry;
 public class Rectangle
 {
+    public double Width;
+    public double Height;
+    public double Area
+    {
+        get { return Width * Height; }
+    }
+    public double Perimeter
+    {
+        get { return (Width + Height) * 2; }
+    }
+
+    public Rectangle(double width, double height)
+    {
+        if (width <= 0 || height <= 0)
+            throw new ArgumentException("All sides must be greater than zero");
+        
+        Width = width;
+        Height = height;
+    }
 }
