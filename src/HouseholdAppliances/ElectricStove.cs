a4be6d1324653d62bd4c75d045a8a52e9d9878f0
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-08-28 01:57:31 -0300

    feat: class EletricStove

--- src/HouseholdAppliances/ElectricStove.cs
+++ src/HouseholdAppliances/ElectricStove.cs
@@ -1,9 +1,48 @@
 namespace HouseholdAppliances;
 
-public class ElectricStove
+public class ElectricStove : Appliance, ICooker
 {
-    public ElectricStove(string alterarEsseNome, string alterarEsseNome2, int alterarEsseNome3, int alterarEsseNome4)
+    public int BoilingTime { get; set; }
+    public int MaximumTemperature { get; set; }
+    public ElectricStove(
+        string Brand, string Model, int boilingTime, int maximumTemperature, bool IsOn = false
+        ) : base(Brand, Model, IsOn)
     {
+        BoilingTime = boilingTime;
+        MaximumTemperature = maximumTemperature;
+    }
 
+    public void Cook(string food)
+    {
+        try
+        {
+            if (!IsOn)
+                throw new ArgumentException("device off");
+            
+            Console.WriteLine($"let's cook {food}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex);
+            throw ex;
+        }
+        
+    }
+
+    public void Bake(string food)
+    {
+        try
+        {
+            if (!IsOn)
+                throw new ArgumentException("device off");
+            
+            Console.WriteLine($"let's bake {food}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex);
+            throw ex;
+        }
+        
     }
 }
