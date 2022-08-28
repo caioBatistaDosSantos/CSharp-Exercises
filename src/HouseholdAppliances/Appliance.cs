119a7d52609fa31893d8b0efe7424200539c7a40
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-08-28 01:27:36 -0300

    feat/test: class abstarct Appliance

--- src/HouseholdAppliances/Appliance.cs
+++ src/HouseholdAppliances/Appliance.cs
@@ -1,5 +1,15 @@
 namespace HouseholdAppliances;
 
-public class Appliance
+public abstract class Appliance
 {
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public bool IsOn { get; set; }
+
+    public Appliance(string brand, string model, bool isOn)
+    {
+        Brand = brand;
+        Model = model;
+        IsOn = isOn;
+    }
 }
